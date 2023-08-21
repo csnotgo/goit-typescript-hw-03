d06e8d9a80cfc66940fc9bdecc5b158c26545b12
Kseniia Skarzhynets <ksuksu1002@gmail.com>
2023-08-21 14:00:37 +0300

    add hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  constructor(public door: boolean, private key: Key, private tenants: Person[] = []) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(k: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(false, key);
+  }
+  openDoor(k: Key): void {
+    if (k === key) {
+      this.door === true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +45,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
